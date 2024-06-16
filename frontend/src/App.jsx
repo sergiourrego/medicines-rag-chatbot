a0812976315a33a131792c6decf586c5a8d910fe
sergiourrego <sergiou1923@gmail.com>
2024-06-16 23:42:40 +0100

    feat/ui-updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Markdown from 'marked-react';
 import nhslogo from './assets/nhs_attribution.png'
@@ -6,7 +6,7 @@ import nhslogo from './assets/nhs_attribution.png'
 function App() {
   const [chatHistory, setChatHistory] = useState({"messages": []});
   const [chatCurrent, setChatCurrent] = useState('');
-  const [urlHistory, setURLHistory] = useState([["https://www.nhs.uk/medicines/paracetamol-for-children/how-and-when-to-give-paracetamol-for-children/#how-its-used-previously-dosage-and-administration", "https://www.nhs.uk/medicines/paracetamol-for-children/how-and-when-to-give-paracetamol-for-children/#how-its-used-previously-dosage-and-administration"]]);
+  const [urlHistory, setURLHistory] = useState([]);
 
   const handleSendMessage = async() => {
     if (chatCurrent.trim()) { // Check for empty or whitespace-only input
@@ -20,13 +20,14 @@ function App() {
       await sendChatToAI(updatedChatHistory);
     }
   };
+  
   // dev environment: this request is proxied to backend - see vite.config
   const sendChatToAI = async (chathistory) => {
     try {
       const response = await fetch('/messages', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({input: chathistory, urls: []}),
+        body: JSON.stringify({input: chathistory, urls: urlHistory}),
       });
       console.log(response)
       if (!response.ok) {
@@ -34,7 +35,7 @@ function App() {
       } else {
         let result = await response.json();
         let newChat = result.input;
-        let urlHistory = result.urls;
+        let urlHistory = result.urls.filter(url => url != null); //remove null objects when no url returned
         setURLHistory(urlHistory);
         setChatHistory(newChat);
       }
@@ -44,13 +45,13 @@ function App() {
   };
   // Parse url into title
   const parseTitle = (url) => {
-    let title = url.match(/medicines\/(.+)\/#/)
+    let title = url.match(/medicines\/(.+)\/#/) 
     let formatTitle = title[1]
     .replace(/-/g, " ")
     .replace(/\//g," | ")
     .split(" ")
     .map(word => {
-      if (!/and|to|for/.test(word)){
+      if (!/^and$|^to$|^for$/.test(word)){
         return word[0].toUpperCase() + word.substring(1)
     } else {
         return word
@@ -59,14 +60,21 @@ function App() {
     return formatTitle
   }
 
+  // Enable scroll to bottom of chat when new message
+  const chatRef = useRef(null)
+  useEffect(() => {chatRef.current?.lastElementChild?.scrollIntoView()}, [chatHistory])
+  // also for url display
+  const urlsRef = useRef(null)
+  useEffect(() => {urlsRef.current?.lastElementChild?.scrollIntoView()}, [urlHistory])
+
   return (
     <div className="flex flex-row h-screen bg-slate-200">
       <div className='flex-col w-2/3'>
-        <div className="chat-area overflow-auto p-4 h-5/6 content-end">
+        <div className="chat-area overflow-y-auto snap-y p-4 h-5/6 content-end" ref={chatRef}>
           {chatHistory.messages.map((message, index) => (
             <div
               key={index}
-              className={`chat ${message.role === 'user' ? 'chat-end' : 'chat-start'}`}
+              className={`snap-end chat ${message.role === 'user' ? 'chat-end' : 'chat-start'}`}
             >
               <div className={`chat-bubble ${message.role === 'user' ? 'bg-slate-500' : 'bg-slate-800'}`}><Markdown>{message.content}</Markdown></div>
             </div>
@@ -85,11 +93,11 @@ function App() {
         </div>
       </div>
       <div className="flex-col content-end w-1/3 bg-slate-400 border-x-2 border-slate-500">
-        <div className="info-urls h-5/6 content-end">
+        <div className="info-urls h-5/6 content-end overflow-y-auto snap-y" ref={urlsRef}>
           {urlHistory.map((urls, index) => (
             <ul key={index} className="menu menu-dropdown bg-blue-100 rounded-lg m-1">
               {urls.map((url, index)=>(
-                <li key={index} className= {index === 0 ? 'bg-blue-300 rounded-md border border-blue-800' : 'bg-blue-300 rounded-md border border-blue-800 mt-2'}>
+                <li key={index} className= {`bg-blue-300 rounded-md border border-blue-800 ${index != 0 && 'mt-2'}`}>
                   <a href={url} target='_blank' className="text-blue-900 font-bold hover:text-blue-600">{parseTitle(url)}</a>
                 </li>
               ))}
